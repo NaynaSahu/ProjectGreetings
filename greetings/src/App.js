14ce6ef9d4182246368fc28edba70a7fcd84cc2a
NaynaSahu <sahu.nayna2001@gmail.com>
2023-02-21 10:28:45 +0530

    added

---
+++ greetings/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let myVariable = 45;
+  return (
+    <>
+    <h1>{myVariable}</h1>
+    </> 
+  );
+}
+
+export default App;
